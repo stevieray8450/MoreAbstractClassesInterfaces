77dc16361f3e2dae2b16c36611393904b8b7d181
stevieray8450 <stevieray8450@gmail.com>
2017-03-16 08:45:35 -0500

    first commit'

--- MoreAbstractClassesInterfaces/Program.cs
+++ MoreAbstractClassesInterfaces/Program.cs
@@ -6,6 +6,46 @@ using System.Threading.Tasks;
 
 namespace MoreAbstractClassesInterfaces
 {
+    abstract class Device
+    {
+        string name;
+        string description;
+        string manufacturer;
+        int numOfInputs;
+        bool hasScreen;
+        bool isHandHeld;
+        bool hasBattery;
+        bool isInternetEnabled;
+        
+        public virtual void MainTask(string name)
+        {
+            Console.WriteLine("Device {0}\'s task implemented here.", name);
+        }
+    }
+
+    interface IText
+    {
+        void Type();
+        void Text();
+        void Send();
+    } 
+
+    interface ICall
+    {
+        void Call();
+        void Answer();
+    }
+
+    interface IPrint
+    {
+        void Print();
+    }
+
+    class Smartphone : Device, IText, ICall
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
